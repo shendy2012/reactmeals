b6cc3f3283ece0984c2fe0a9a130f3f971390ac2
shendy2012 <gudilov94@gmail.com>
2022-10-01 17:47:21 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+  const [cartIsShom, setcartIsShom] = useState(false)
+  const showCartHandler = () => {
+    setcartIsShom(true)
+  }
+  const hideCartHandler = () => {
+    setcartIsShom(false)
+  }
+
+
+  return (
+    <CartProvider>
+      {cartIsShom && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
